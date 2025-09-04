2126a77d841021e63f4873db5ecb61b0b5b5a397
alex-olave <hipsterkatten@gmail.com>
2025-09-04 23:52:14 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/harmonia/' : '/',
+  base: "/harmonia-webb/",
   server: {
     host: "::",
     port: 8080,
